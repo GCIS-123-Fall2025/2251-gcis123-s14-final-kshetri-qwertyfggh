fce8ab4945093006b8c89dbb75f4b04ee090176b
Bilolbek Butaboev (RIT Student) <bb1881@rit.edu>
2025-12-15 10:03:27 -0500

    Question 3 finished (wasn't able to get stack to work)

--- balance_parenthesis.py
+++ balance_parenthesis.py
@@ -18,13 +18,38 @@ Examples:
 "-----() -- ( () )" returns 0
 
 """
-from node_stack import Stack
+from node_stack import Stack #Changed method names in the node_stack.py file itself because .push() wasn't working (Nvm its not working anyways, I don't know why its not working)
+
+#This should work hopefully, I wasn't able to test because the stack module wouldnt work
 
 def balance_parenthesis(a_string):
-    pass # please replace with your solution
+    nstack = Stack
+    i = 0
+    for letter in a_string:
+        if letter == "(" or letter == ")":
+            nstack.pu(letter)
+            i += 1
+    
+    x = 0
+    lastparenthesis = 0
+    for n in range(i):
+        p = nstack.po()
+        if p == "(":
+            x += 1
+        if p == ")":
+            x -= 1
+            lastparenthesis = i-(n+1)
+    if x == 0:
+        return 0
+    elif x > 0:
+        return lastparenthesis
+    elif x < 0:
+        return -1
+    
 
 
-def main():     pass
- 
+
+def main():
+    balance_parenthesis("()----)")
 
 if __name__ == "__main__":    main()
